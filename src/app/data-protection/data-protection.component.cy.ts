8978743e9f9d980824081d39137e074a53d29873
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2023-05-26 18:01:39 +0200

    imprint and data protection

---
+++ src/app/data-protection/data-protection.component.cy.ts
@@ -0,0 +1,7 @@
+import { DataProtectionComponent } from './data-protection.component'
+
+describe('DataProtectionComponent', () => {
+  it('should mount', () => {
+    cy.mount(DataProtectionComponent)
+  })
+})
